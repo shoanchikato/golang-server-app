ecb27f0f193a9f9a201f05a168c57cd394961f79
Shoan Chikato <sjchikato@gmail.com>
2024-09-05 13:40:04 +0200

    add graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	d "app/pkg/di"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -11,7 +15,16 @@ func main() {
 	app := d.Di()
 	defer app.DB.Close()
 
-	if err := app.App.Listen(":3000"); err != nil {
-		log.Fatal("App stopped", err)
-	}
+	go func() {
+		if err := app.App.Listen(":3000"); err != nil {
+			log.Fatal("App stopped", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigChan
+	fmt.Println("\nReceived signal:", sig)
+
+	fmt.Println("Application shutting down...")
 }

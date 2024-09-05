69b1150b28a4809744eb04a6fdb615543c3094e2
Shoan Chikato <sjchikato@gmail.com>
2024-09-05 13:43:32 +0200

    add utility file for generic handler functions

---
+++ pkg/handler/util.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func getId(c *fiber.Ctx) (*int, error) {
+	idStr := c.Params("id")
+
+	int, err := strconv.Atoi(idStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &int, nil
+}
+
+func getAuthUserId(c *fiber.Ctx) (*int, error) {
+	ctx := c.UserContext()
+	userIdKey := userContextKey("userId")
+	userId := ctx.Value(userIdKey).(int)
+	if userId == 0 {
+		return nil, errors.New("user not authorized: user unknown")
+	}
+
+	return &userId, nil
+
+}

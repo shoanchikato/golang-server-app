5967ada88f494d3df0e73bc4f90350bb2902e255
Shoan Chikato <sjchikato@gmail.com>
2024-09-07 04:17:40 +0200

    add a guard in HttpErrorFmt for checking nil errors

--- pkg/service/error_fmt.go
+++ pkg/service/error_fmt.go
@@ -20,31 +20,36 @@ func NewHttpErrorFmt() HttpErrorFmt {
 // GetError
 func (er *httpErrorFmt) GetError(err error) error {
 
-	validationErr := &e.ValidationError{}
-	notFoundErr := &e.RepoNotFoundError{}
-	duplicateErr := &e.RepoDuplicateError{}
+	if err != nil {
+		
+		validationErr := &e.ValidationError{}
+		notFoundErr := &e.RepoNotFoundError{}
+		duplicateErr := &e.RepoDuplicateError{}
 
-	switch {
-	case errors.Is(err, e.ErrIncorrectCredentials):
-		log.Println(err)
-		return e.NewHttpError("incorrect username or password", http.StatusUnauthorized)
-	case errors.Is(err, e.ErrNotAuthorized):
-		log.Println(err)
-		return e.NewHttpError("user is not authorized", http.StatusUnauthorized)
-	case errors.Is(err, e.ErrInvalidToken):
-		log.Println(err)
-		return e.NewHttpError("token is invalid", http.StatusUnauthorized)
-	case errors.Is(err, e.ErrTokenExpired):
-		log.Println(err)
-		return e.NewHttpError(err.Error(), http.StatusUnauthorized)
-	case errors.As(err, &validationErr):
-		return e.NewHttpError(validationErr.ErrStr, http.StatusBadRequest)
-	case errors.As(err, &duplicateErr):
-		return e.NewHttpError(duplicateErr.ErrStr, http.StatusBadRequest)
-	case errors.As(err, &notFoundErr):
-		return e.NewHttpError(notFoundErr.ErrStr, http.StatusNotFound)
-	default:
-		log.Println("Server error", err)
-		return e.NewHttpError("server error", http.StatusInternalServerError)
+		switch {
+		case errors.Is(err, e.ErrIncorrectCredentials):
+			log.Println(err)
+			return e.NewHttpError("incorrect username or password", http.StatusUnauthorized)
+		case errors.Is(err, e.ErrNotAuthorized):
+			log.Println(err)
+			return e.NewHttpError("user is not authorized", http.StatusUnauthorized)
+		case errors.Is(err, e.ErrInvalidToken):
+			log.Println(err)
+			return e.NewHttpError("token is invalid", http.StatusUnauthorized)
+		case errors.Is(err, e.ErrTokenExpired):
+			log.Println(err)
+			return e.NewHttpError(err.Error(), http.StatusUnauthorized)
+		case errors.As(err, &validationErr):
+			return e.NewHttpError(validationErr.ErrStr, http.StatusBadRequest)
+		case errors.As(err, &duplicateErr):
+			return e.NewHttpError(duplicateErr.ErrStr, http.StatusBadRequest)
+		case errors.As(err, &notFoundErr):
+			return e.NewHttpError(notFoundErr.ErrStr, http.StatusNotFound)
+		default:
+			log.Println("Server error", err)
+			return e.NewHttpError("server error", http.StatusInternalServerError)
+		}
 	}
+
+	return nil
 }

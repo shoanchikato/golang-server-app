7d6c5d8eb4adfd14d9be9bfd4d31e5f1092f2aa5
Shoan Chikato <sjchikato@gmail.com>
2024-09-07 07:36:07 +0200

    change order of routes

--- pkg/route/permission_management_route.go
+++ pkg/route/permission_management_route.go
@@ -10,10 +10,10 @@ import (
 func NewPermissionManagementRoutes(app *fiber.App, handler h.PermissionManagementHandler, middleware mi.AuthMiddleware) {
 	permissionManagement := app.Group("/permission-management")
 	permissionManagement.Use(middleware.JWTParser)
-	permissionManagement.Post("/:permissionId/:roleId", handler.AddPermissionToRole)
-	permissionManagement.Post("/:roleId", handler.AddPermissionsToRole)
 	permissionManagement.Get("/role/:roleId", handler.GetPermissionsByRoleId)
 	permissionManagement.Get("/user/:userId", handler.GetPermissionsByUserId)
-	permissionManagement.Delete(":permissionId/:roleId", handler.RemovePermissionFromRole)
+	permissionManagement.Post("/:roleId", handler.AddPermissionsToRole)
+	permissionManagement.Post("/:permissionId/:roleId", handler.AddPermissionToRole)
 	permissionManagement.Delete("/:roleId", handler.RemovePermissionsFromRole)
+	permissionManagement.Delete(":permissionId/:roleId", handler.RemovePermissionFromRole)
 }

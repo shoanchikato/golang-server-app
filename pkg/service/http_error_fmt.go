ef8a39140b718998f007ac8ad93b0a0674fde5dc
Shoan Chikato <sjchikato@gmail.com>
2024-09-07 04:49:39 +0200

    add json structured logging

--- pkg/service/http_error_fmt.go
+++ pkg/service/http_error_fmt.go
@@ -3,8 +3,10 @@ package service
 import (
 	e "app/pkg/errors"
 	"errors"
-	"log"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 type HttpErrorFmt interface {
@@ -21,23 +23,26 @@ func NewHttpErrorFmt() HttpErrorFmt {
 func (er *httpErrorFmt) GetError(err error) error {
 
 	if err != nil {
-		
+
 		validationErr := &e.ValidationError{}
 		notFoundErr := &e.RepoNotFoundError{}
 		duplicateErr := &e.RepoDuplicateError{}
 
+		jsonHandler := slog.NewJSONHandler(os.Stdout, nil)
+		logger := slog.New(jsonHandler)
+
 		switch {
 		case errors.Is(err, e.ErrIncorrectCredentials):
-			log.Println(err)
+			logger.Error(err.Error())
 			return e.NewHttpError("incorrect username or password", http.StatusUnauthorized)
 		case errors.Is(err, e.ErrNotAuthorized):
-			log.Println(err)
+			logger.Error(err.Error())
 			return e.NewHttpError("user is not authorized", http.StatusUnauthorized)
 		case errors.Is(err, e.ErrInvalidToken):
-			log.Println(err)
+			logger.Error(err.Error())
 			return e.NewHttpError("token is invalid", http.StatusUnauthorized)
 		case errors.Is(err, e.ErrTokenExpired):
-			log.Println(err)
+			logger.Error(err.Error())
 			return e.NewHttpError(err.Error(), http.StatusUnauthorized)
 		case errors.As(err, &validationErr):
 			return e.NewHttpError(validationErr.ErrStr, http.StatusBadRequest)
@@ -46,7 +51,7 @@ func (er *httpErrorFmt) GetError(err error) error {
 		case errors.As(err, &notFoundErr):
 			return e.NewHttpError(notFoundErr.ErrStr, http.StatusNotFound)
 		default:
-			log.Println("Server error", err)
+			logger.Error(fmt.Sprintf("Server error: %v", err))
 			return e.NewHttpError("server error", http.StatusInternalServerError)
 		}
 	}

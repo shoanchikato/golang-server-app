3eef2d508e8e4aa75dd3f585d1d7335f6f44f630
Shoan Chikato <sjchikato@gmail.com>
2024-09-17 21:43:39 +0200

    add more test setup functionality

--- test/setup/get_error_response.go
+++ test/setup/get_error_response.go
@@ -2,18 +2,30 @@ package setup
 
 import (
 	"encoding/json"
-	"io"
+	"net/http"
 	"testing"
 )
 
-func GetErrorResponse(t *testing.T, reader io.Reader) *map[string]any {
+func GetErrorResponse(t *testing.T, resp *http.Response) *map[string]any {
 	value := &map[string]any{}
+	
+	err := json.NewDecoder(resp.Body).Decode(value)
+	if err == nil {
+		return value
+	}
+	
+	stringValue := ""
 
-	err := json.NewDecoder(reader).Decode(value)
-	if err != nil {
+	switch {
+	case resp.StatusCode >= 400 && resp.StatusCode < 600:
+		stringValue = http.StatusText(resp.StatusCode)
+	default:
 		t.Error("error decoding error message: ", err)
-		return nil
 	}
 
-	return value
+	return &map[string]any{
+		"error": map[string]string{
+			"message": stringValue,
+		},
+	}
 }

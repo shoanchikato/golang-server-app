e5482019f554cec48efc27e8ff5dd33479247bae
Shoan Chikato <sjchikato@gmail.com>
2025-12-14 11:47:29 +0200

    update url for add permission to role test case

--- test/endpoints/permissionmanagement/positive_test.go
+++ test/endpoints/permissionmanagement/positive_test.go
@@ -91,7 +91,7 @@ func Test_Permission_Management_Endpoint__Positive_Test(t *testing.T) {
 		expectStatus := http.StatusNoContent
 
 		// act
-		req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/permission-management/%d/%d", permissionId, roleId), nil)
+		req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/permission-management/permission/%d/role/%d", permissionId, roleId), nil)
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Authorization", tokens.Access)
 
